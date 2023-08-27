b25685d20b1230263b5e0a91f50f52dfea3f2475
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-08-27 21:11:22 +0300

    removed unless selectors

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,7 +1,1 @@
-export const millisecondsSelector = (state) => state.stopwatch.milliseconds;
-export const secondsSelector = (state) => state.stopwatch.seconds;
-export const minutesSelector = (state) => state.stopwatch.minutes;
-export const hoursSelector = (state) => state.stopwatch.hours;
-
-export const isResetSelector = (state) => state.stopwatch.isReset;
 export const isStartSelector = (state) => state.stopwatch.isStart;
